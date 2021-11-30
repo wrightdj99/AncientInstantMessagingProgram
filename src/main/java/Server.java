5d9278bdc6441871893b0bf64276375509708faa
Dan Wright <wrightdj99@gmail.com>
2021-11-30 15:57:54 -0600

    initial commit

---
+++ src/main/java/Server.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.net.*;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class Server extends JFrame{
+    //UI elements that include our client text field and chat window
+    private JTextField userText;
+    private JTextArea ourWindow;
+    //Networking in and out streams
+    private ObjectOutputStream output;
+    private ObjectInputStream input;
+    //Server side socket where we can receive messages from the client
+    private ServerSocket server;
+    private Socket connection;
+}
